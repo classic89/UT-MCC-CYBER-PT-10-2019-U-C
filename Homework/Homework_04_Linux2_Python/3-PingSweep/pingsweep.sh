855a1c1b5f4abdcf6032c4b5debd7975013d94c4
Megan Wilson <retrorock8821@gmail.com>
2020-04-09 10:06:34 -0400

    pingsweep

--- Homework/Homework_04_Linux2_Python/3-PingSweep/pingsweep.sh
+++ Homework/Homework_04_Linux2_Python/3-PingSweep/pingsweep.sh
@@ -6,11 +6,11 @@ set -o errexit # stop execution on error
 
 PREFIX=$1
 echo 'Scanning  $PREFIX.0/24 ... '
-for i in $(seq -f "$PREFIX.%02g" 5)
+for ip in $(seq -f "$PREFIX.%01g" 5)
 do
      echo "$i : "
      TARGET=$i
-
+     ping -c 1 $TARGET
 done
 
 
